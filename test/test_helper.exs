f40a3303f071ab492e66d8d0bcae9f21f83024be
ymtszw <ymtszw@gmail.com>
2017-03-11 17:06:35 +0900

    Switch port for test client

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,9 @@
 ExUnit.start()
 
 defmodule Req do
+  @port System.get_env("PORT") || "4000"
+
   def get(path) do
-    HTTPoison.get("http://localhost:4000" <> path)
+    HTTPoison.get("http://localhost:#{@port}#{path}")
   end
 end
